cf25174fe3b0ae0767c0e366239aa7328a40cae0
Mickez93 <mini0072@student.umu.se>
2023-09-22 15:49:34 +0200

    RTOS Fungerande

---
+++ L4B14/Output/Debug/Obj/L4B14/gpio.d
@@ -0,0 +1,76 @@
+Output/Debug/Obj/L4B14/gpio.o: \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Core/Src/gpio.c \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Core/Inc/gpio.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Core/Inc/main.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Core/Inc/stm32l4xx_hal_conf.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l432xx.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/CMSIS_5/CMSIS/Core/Include/core_cm4.h \
+ /usr/share/segger_embedded_studio_for_arm_7.30/include/stdint.h \
+ /usr/share/segger_embedded_studio_for_arm_7.30/include/__SEGGER_RTL.h \
+ /usr/share/segger_embedded_studio_for_arm_7.30/include/__SEGGER_RTL_ConfDefaults.h \
+ /usr/share/segger_embedded_studio_for_arm_7.30/include/__SEGGER_RTL_Conf.h \
+ /usr/share/segger_embedded_studio_for_arm_7.30/include/__SEGGER_RTL_Arm_Conf.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/CMSIS_5/CMSIS/Core/Include/cmsis_version.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/CMSIS_5/CMSIS/Core/Include/cmsis_gcc.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/CMSIS_5/CMSIS/Core/Include/mpu_armv7.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /usr/share/segger_embedded_studio_for_arm_7.30/include/stddef.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_adc.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_ll_adc.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_adc_ex.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_exti.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim_ex.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/L4B14/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/arm_math.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/arm_math_types.h \
+ /home/micke/Documents/SEGGER\ Embedded\ Studio\ for\ ARM\ Projects/L4B14/CMSIS_5/CMSIS/Core/Include/cmsis_compiler.h \
+ /usr/share/segger_embedded_studio_for_arm_7.30/include/string.h \
+ /usr/share/segger_embedded_studio_for_arm_7.30/include/math.h \
+ /usr/share/segger_embedded_studio_for_arm_7.30/include/__SEGGER_RTL_FP.h \
+ /usr/share/segger_embedded_studio_for_arm_7.30/include/float.h \
+ /usr/share/segger_embedded_studio_for_arm_7.30/include/limits.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/arm_math_memory.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/dsp/none.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/arm_math_types.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/dsp/utils.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/dsp/basic_math_functions.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/arm_math_memory.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/dsp/none.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/dsp/utils.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/dsp/interpolation_functions.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/dsp/bayes_functions.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/dsp/statistics_functions.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/dsp/basic_math_functions.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/dsp/fast_math_functions.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/dsp/matrix_functions.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/dsp/complex_math_functions.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/dsp/statistics_functions.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/dsp/controller_functions.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/dsp/support_functions.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/dsp/distance_functions.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/dsp/svm_functions.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/dsp/svm_defines.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/dsp/fast_math_functions.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/dsp/transform_functions.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/dsp/complex_math_functions.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/dsp/filtering_functions.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/dsp/support_functions.h \
+ /home/micke/.segger/SEGGER\ Embedded\ Studio/v3/packages/CMSIS_5/CMSIS/DSP/Include/dsp/quaternion_math_functions.h
